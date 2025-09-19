4485206cda85b284a57aa84f2cd6980532035c6b
Anchal-Anand <Anchal@edu.sait.ca>
2025-09-19 15:54:22 -0600

    i edited one file, app/page.js and added the week-2 folder with two files, page.js and student-info.js, under app folder.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Anchal</p>
+      <p>
+        <Link href="https://github.com/Anchal-Anand/cprg306-assignments" target="_blank">
+          GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+}
